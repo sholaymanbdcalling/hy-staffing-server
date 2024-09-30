83d88ce9b36934cbe436dd5b46970633e87c1746
Abdus Samad <abdusbdcalling@gmail.com>
2024-09-30 08:51:58 +0600

    is email verified funtionality added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -98,42 +98,58 @@ const verifyEmail = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  //   find user
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new ApiError(404, 'User does not exist');
+    //   find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new ApiError(404, 'User does not exist');
+    }
+
+    // Is email verified
+    if (user?.otp !== null) {
+      throw new ApiError(404, 'Email is not verified');
+    }
+
+    //   check is password valid
+    const isPasswordValid = await user.isValidPassword(password);
+    if (!isPasswordValid) {
+      throw new ApiError(401, 'Invalid user credentials');
+    }
+
+    const token = await generateToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select('-password -otp');
+
+    const option = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie('token', token, option)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            user: loggedInUser,
+            token,
+          },
+          'User logged in successfully',
+        ),
+      );
+  } catch (error) {
+    console.error('Error during user login:', error);
+
+    // Return an appropriate error response based on the error type
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json({ message: error.message });
+    }
+
+    return res.status(500).json({ message: 'An unexpected error occurred during user login' });
   }
-
-  //   check is password valid
-  const isPasswordValid = await user.isValidPassword(password);
-  if (!isPasswordValid) {
-    throw new ApiError(401, 'Invalid user credentials');
-  }
-
-  const token = await generateToken(user._id);
-
-  const loggedInUser = await User.findById(user._id).select('-password -otp');
-
-  const option = {
-    httpOnly: true,
-    secure: true,
-  };
-
-  return res
-    .status(200)
-    .cookie('token', token, option)
-    .json(
-      new ApiResponse(
-        200,
-        {
-          user: loggedInUser,
-          token,
-        },
-        'User logged in successfully',
-      ),
-    );
 };
 
 const logoutUser = async (req, res) => {
